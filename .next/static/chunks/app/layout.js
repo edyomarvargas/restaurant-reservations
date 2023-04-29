cf7af32c9b4d2545a82ad922205a8f2201481fa9
edyomarvargas <edyomar-vargas@hotmail.com>
2023-04-29 17:44:51 -0300

    initial commit

---
+++ .next/static/chunks/app/layout.js
@@ -0,0 +1,28 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/layout"],{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fhome%2Fedyomar%2F%C3%81rea%20de%20Trabalho%2FProjetos%20GitHub%2Frestaurant-reservation%2Fapp%2Fglobals.css&server=false!":
+/*!******************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fhome%2Fedyomar%2F%C3%81rea%20de%20Trabalho%2FProjetos%20GitHub%2Frestaurant-reservation%2Fapp%2Fglobals.css&server=false! ***!
+  \******************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("(() => __webpack_require__.e(/*! import() */ \"_app-client_app_globals_css\").then(__webpack_require__.bind(__webpack_require__, /*! ./app/globals.css */ \"(app-client)/./app/globals.css\")))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPSUyRmhvbWUlMkZlZHlvbWFyJTJGJUMzJTgxcmVhJTIwZGUlMjBUcmFiYWxobyUyRlByb2pldG9zJTIwR2l0SHViJTJGcmVzdGF1cmFudC1yZXNlcnZhdGlvbiUyRmFwcCUyRmdsb2JhbHMuY3NzJnNlcnZlcj1mYWxzZSEuanMiLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxtTEFBeUgiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz9lZDZiIl0sInNvdXJjZXNDb250ZW50IjpbIigoKSA9PiBpbXBvcnQoLyogd2VicGFja01vZGU6IFwibGF6eVwiICovIFwiL2hvbWUvZWR5b21hci/DgXJlYSBkZSBUcmFiYWxoby9Qcm9qZXRvcyBHaXRIdWIvcmVzdGF1cmFudC1yZXNlcnZhdGlvbi9hcHAvZ2xvYmFscy5jc3NcIikpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fhome%2Fedyomar%2F%C3%81rea%20de%20Trabalho%2FProjetos%20GitHub%2Frestaurant-reservation%2Fapp%2Fglobals.css&server=false!\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fhome%2Fedyomar%2F%C3%81rea%20de%20Trabalho%2FProjetos%20GitHub%2Frestaurant-reservation%2Fapp%2Fglobals.css&server=false!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
