cd34173b8b762b4c37d920e47e598a0d162627a9
edyomarvargas <edyomar-vargas@hotmail.com>
2023-05-01 18:41:44 -0300

    feat: create function to fetch restaurant menu data

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,12 +1,37 @@
-import Header from "../components/Header";
 import Menu from "../components/Menu";
 import RestaurantNavBar from "../components/RestaurantNavBar";
+import { PrismaClient } from "@prisma/client";
 
-export default function RestaurantMenu() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantMenu = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      item: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant.item;
+};
+
+export default async function RestaurantMenu({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const menu = await fetchRestaurantMenu(params.slug);
+
   return (
     <div className="bg-white w-[100%] rounded p-3 shadow">
-      <RestaurantNavBar />
-      <Menu />
+      <RestaurantNavBar slug={params.slug} />
+      <Menu menu={menu} />
     </div>
   );
 }
