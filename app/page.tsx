e08e71846c142d66cbb199f658a47e79d8c802b4
edyomarvargas <edyomar-vargas@hotmail.com>
2023-05-01 17:42:29 -0300

    feat: render restaurant info dynamically

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,47 @@
 import Header from "./components/Header";
 import RestauranteCard from "./components/RestaurantCard";
+import { PrismaClient, Cuisine, Location, PRICE } from "@prisma/client";
 
-export default function Home() {
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+  slug: string;
+}
+
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+      slug: true,
+    }
+  });
+  return restaurants;
+}
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+
+  console.log({ restaurants });
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        <RestauranteCard />
+        {
+          restaurants.map((restaurant) => (
+            <RestauranteCard restaurant={restaurant} />
+          ))
+        }
       </div>
     </main>
   );
