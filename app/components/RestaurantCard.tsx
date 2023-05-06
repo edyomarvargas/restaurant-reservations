7b5a5b534e9c7d0131b4651e77fc4a1f3159503a
edyomarvargas <edyomar-vargas@hotmail.com>
2023-05-06 15:53:35 -0300

    feat: make reviews dynamic on RestaurantCard component

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -6,7 +6,7 @@ interface Props {
   restaurant: RestaurantCardType;
 }
 
-export default function RestauranteCard({restaurant}: Props) {
+export default function RestauranteCard({ restaurant }: Props) {
   return (
     <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
       <Link href={`/restaurant/${restaurant.slug}`}>
@@ -19,7 +19,10 @@ export default function RestauranteCard({restaurant}: Props) {
           <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
           <div className="flex items-start">
             <div className="flex mb-2">*****</div>
-            <p className="ml-2">77 reviews</p>
+            <p className="ml-2">
+              {restaurant.reviews.length} review
+              {restaurant.reviews.length === 1 ? "" : "s"}
+            </p>
           </div>
           <div className="flex text-reg font-light capitalize">
             <p className=" mr-3">{restaurant.cuisine.name}</p>
