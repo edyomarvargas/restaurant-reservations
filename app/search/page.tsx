fd4c4c917cf4d2f5419989d9fef2a7365f7afebd
edyomarvargas <edyomar-vargas@hotmail.com>
2023-05-05 23:22:08 -0300

    feat: create fetchRestaurantsByCity function

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,41 @@
+import { PrismaClient } from "@prisma/client";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 import SearchSideBar from "./components/SearchSideBar";
 
-export default function Search() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantsByCity = (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+  };
+
+  if (!city) prisma.restaurant.findMany({ select });
+
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city?.toLowerCase(),
+        },
+      },
+    },
+    select,
+  });
+};
+
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: { city: string };
+}) {
+  const restaurants = await fetchRestaurantsByCity(searchParams.city);
   return (
     <>
       <Header />
@@ -10,7 +43,15 @@ export default function Search() {
         <SearchSideBar />
 
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length ? (
+            <>
+              {restaurants.map((restaurant) => (
+                <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+              ))}
+            </>
+          ) : (
+            <p>No restaurants found in this area...</p>
+          )}
         </div>
       </div>
     </>
