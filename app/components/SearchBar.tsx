fd42daed5fffa43a5a03a8995e6452d3e8a25859
edyomarvargas <edyomar-vargas@hotmail.com>
2023-05-05 23:21:37 -0300

    feat: make the navigation of search bar dynamic

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -18,7 +18,11 @@ export default function SearchBar() {
       />
       <button
         className="rounded bg-red-600 px-9 py-2 text-white"
-        onClick={() => router.push("/search")}
+        onClick={() => {
+          if (location === "") return;
+          router.push(`/search?city=${location}`);
+          setLocation("");
+        }}
       >
         Let's go
       </button>
