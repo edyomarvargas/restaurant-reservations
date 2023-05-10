3eae068f1d15a9756532804983bf33db47024150
edyomarvargas <edyomar-vargas@hotmail.com>
2023-05-10 16:10:50 -0300

    refactor: change name of LoginModal component to AuthModal

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -2,8 +2,6 @@
 
 import { useState } from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 
 const style = {
@@ -13,7 +11,6 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
@@ -44,14 +41,7 @@ export default function LoginModal({ isSignin }: { isSignin: boolean }) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
+        <Box sx={style}></Box>
       </Modal>
     </div>
   );
